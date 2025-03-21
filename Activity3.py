c22323edc8086a40fcd8ad2d1ffc6b7f2258e6d3
whostausif <tausifwali@gmail.com>
2025-03-21 12:00:38 +0600

    lesson 5 activity 3

---
+++ Activity3.py
@@ -0,0 +1,8 @@
+import math
+print("The floor and ceiling value of 23.56 are: "+ str(math.ceil(23.56))+","+str (math.floor(23.56)))
+x = -25
+y = 11
+print("The value of x after copying the sign of y is: "+str(math.copysign(x,y)))
+
+print("Absolute value of -25 and 11 are: "+str(math.fabs(-25))+" and "+str(math.fabs(11)))
+print("The GCD of 19 and 22 are: "+str(math.gcd(19,22)))
